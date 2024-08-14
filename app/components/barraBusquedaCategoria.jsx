f28922aadee0a38268aee12ff0329e7ed2b26229
Zaywop06 <160089073+Zaywop06@users.noreply.github.com>
2024-08-14 10:45:02 -0600

    Primer commit del proyecto React Native

---
+++ app/components/barraBusquedaCategoria.jsx
@@ -0,0 +1,60 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import React from 'react';
+import { TextInput, View, StyleSheet, Dimensions } from 'react-native';
+const { width: viewportWidth } = Dimensions.get("window");
+
+const BarraBusquedaCategoria = ({ busquedaCategoria, setbusquedaCategoria }) => {
+    return (
+        <View style={estilos.contenedorBusqueda}>
+            <View style={estilos.barraBusqueda}>
+                <FontAwesome5 name="search" size={25} color="#222C57" />
+                <TextInput
+                    style={estilos.entradaBusqueda}
+                    placeholder="¿Qué desea buscar hoy?"
+                    placeholderTextColor="#222C57"
+                    value={busquedaCategoria}
+                    onChangeText={setbusquedaCategoria}
+                />
+                {busquedaCategoria ? (
+                    <FontAwesome5
+                        name="times"
+                        size={25}
+                        color="#C61919"
+                        onPress={() => setbusquedaCategoria("")}
+                    />
+                ) : null}
+            </View>
+        </View>
+    )
+}
+
+const estilos = StyleSheet.create({
+    contenedorBusqueda: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: viewportWidth * 0.9,
+        alignSelf: "center",
+        marginVertical: 10,
+    },
+    barraBusqueda: {
+        flexDirection: "row",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: "#FFF",
+        borderRadius: 20,
+        paddingHorizontal: 15,
+        paddingVertical: 8,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        elevation: 3,
+    },
+    entradaBusqueda: {
+        marginStart: 15,
+        flex: 1,
+        fontSize: 16,
+    },
+})
+export default BarraBusquedaCategoria;
